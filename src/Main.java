946680acb2fcba227c4de3074d092bf44857be92
rashmoni <rashudey9851@gmail.com>
2022-07-29 21:29:45 +0200

    added character encoding for windows

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("file.encoding", "UTF-8");
         //Create object of Application and initiate mainMenu
         Application app1 = new Application();
         app1.mainMenu();
